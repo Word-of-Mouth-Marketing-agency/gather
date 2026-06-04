f2e14f79e89e5c20b799c3c2de024b23b93bfe7a
Mero Yasser <meroprotech975@gmail.com>
2026-06-04 14:54:35 +0300

    fix: sticky header, bigger logo h-12/lg:h-14

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
   const closeMobile = () => setMobileOpen(false)
 
   return (
-    <header className="relative z-50 bg-white border-b border-gray-100">
+    <header className="sticky top-0 z-50 bg-white border-b border-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 lg:h-20">
           {/* Logo */}
@@ -44,7 +44,7 @@ export default function Navbar() {
             <img
               src="/assets/gather/gather-logo.webp"
               alt="Gather"
-              className="h-10 lg:h-12 w-auto"
+              className="h-12 lg:h-14 w-auto"
             />
           </Link>
 
