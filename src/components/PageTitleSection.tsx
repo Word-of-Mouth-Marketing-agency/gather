723f21bb60b7d4c222a4c1aaecd5160f2d663d9a
Mero Yasser <meroprotech975@gmail.com>
2026-06-10 10:39:29 +0300

    style: reduce page title section sizing

--- src/components/PageTitleSection.tsx
+++ src/components/PageTitleSection.tsx
@@ -8,12 +8,12 @@ interface Props {
 export default function PageTitleSection({ title, accentWord }: Props) {
   return (
     <section className="bg-[#F3E7D9] w-full">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-20 lg:py-24 text-center">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-16 lg:py-18 text-center">
         <AnimatedTitle
           as="h1"
           text={title}
           accentWord={accentWord}
-          className="text-4xl sm:text-5xl lg:text-6xl font-black text-[#171717]"
+          className="text-3xl sm:text-4xl lg:text-5xl font-black text-[#171717]"
         />
       </div>
     </section>
